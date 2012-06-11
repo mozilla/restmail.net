44306506b1a33ed3c1fbc1b61f13b8d557b80141
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-06-11 16:53:40 +0300

    add iptables routing to post_create.sh script so incoming mail works

--- aws_scripts/post_create.sh
+++ aws_scripts/post_create.sh
@@ -7,3 +7,6 @@ sudo make install
 cd utils
 sudo ln -s /usr/local/bin/redis-server /usr/bin/redis-server
 sudo ./install_server.sh
+
+# now route port 25 to 9025 so that an unpriviledge process can handle mail
+sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 25 -j REDIRECT --to-ports 9025
